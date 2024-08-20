d493cf759ddcb5967aa40a561dacfe5595f99af2
sandoval <sandovalbgasantiago@gmail.com>
2024-08-20 22:26:51 +0200

    ArrayList /lust-details

--- src/main/java/com/intro/intro/controllers/UserRestController.java
+++ src/main/java/com/intro/intro/controllers/UserRestController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.intro.intro.models.User;
 import com.intro.intro.models.dto.UserDTO;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +26,18 @@ public class UserRestController {
         userDto.setTitle("Second try with SpringBoot");
         userDto.setUser(user);
         return userDto;
+    }
+
+    @GetMapping("/list-details")
+    public List<User> listdetails(){
+        User user = new User("Emmanuel", "Gazmey");
+        User user2 = new User("Re","Malo");
+        User user3 = new User("Post","Malon");
+        List<User> listUsers = new ArrayList<>();
+        listUsers.add(user);
+        listUsers.add(user2);
+        listUsers.add(user3);
+        return listUsers;
     }
     
 
